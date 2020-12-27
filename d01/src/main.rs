b99feab50a45de5f594d6fb8fd335ea19931398b
dan <dancrhorton@gmail.com>
2020-12-26 22:53:04 -0500

    day 1 mega optimised

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -1,20 +1,63 @@
-use aoc_2020::get_input_as_int;
-use itertools::Itertools;
+use aoc_2020::{get_input_as_int, get_input_as_int_set};
+use itertools::{cons_tuples, Itertools};
+use std::cmp::Ordering;
+use std::collections::HashSet;
+use std::time::Instant;
 
 fn main() {
-    let input = get_input_as_int("d01.txt");
+    let input = get_input_as_int_set("1");
 
-    for values in input.iter().combinations(2) {
-        if values.iter().map(|i| *i).sum::<i64>() == 2020 {
-            println!("Part 1: {}", values.into_iter().product::<i64>());
-            break;
+    let start = Instant::now();
+
+    let f = run1_alt(&input);
+    let g = run2_alt(&input);
+
+    let t = start.elapsed().as_micros();
+
+    println!("Part 1: {}", f);
+    println!("Part 2: {}", g);
+    println!("Duration: {:.3}us", t);
+}
+
+fn run1_alt(numbers: &HashSet<i64>) -> i64 {
+    let mut part = 0;
+    for v in numbers {
+        let u = 2020 - v;
+        if numbers.contains(&u) {
+            part = u * v
         }
     }
+    part
+}
 
-    for values in input.iter().combinations(3) {
-        if values.iter().map(|i| *i).sum::<i64>() == 2020 {
-            println!("Part 2: {}", values.into_iter().product::<i64>());
-            break;
+fn run2_alt(numbers: &HashSet<i64>) -> i64 {
+    let mut part = 0;
+    // for some reason this is faster than combinations
+    for i in numbers {
+        for j in numbers {
+            if i == j {
+                continue;
+            }
+            let v = *i + *j;
+            if v >= 2020 {
+                continue;
+            }
+            let u = 2020 - v;
+            if numbers.contains(&u) {
+                part = *i * *j * u;
+            }
         }
     }
+    // for values in numbers.iter().combinations(2) {
+    //     let (i, j) = values.iter().next_tuple().unwrap();
+    //     let v = *i + *j;
+    //     if v >= 2020 {
+    //         continue;
+    //     }
+    //     let u = 2020 - v;
+    //     if numbers.contains(&u) {
+    //         part = *i * *j * u;
+    //     }
+    // }
+    part
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
+use std::collections::HashSet;
 use std::fmt::Debug;
 use std::fs::File;
+use std::hash::Hash;
 use std::io::{BufRead, BufReader};
 use std::ops::{Add, Mul, Sub};
 use std::str::FromStr;
 
 pub fn get_input(filename: &str) -> Vec<String> {
-    let file = match File::open(format!("input/{}", filename)) {
+    let file = match File::open(format!(
+        "/home/dan/PycharmProjects/advent/inputs/input{}2020",
+        filename
+    )) {
         Ok(file) => file,
         Err(error) => panic!("Unable to open file {}: {}", filename, error),
     };
@@ -25,4 +30,17 @@ where
         .iter()
         .map(|i| i.parse().unwrap())
         .collect()
+}
+pub fn get_input_as_int_set<
+    T: Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Ord + FromStr + Hash,
+>(
+    filename: &str,
+) -> HashSet<T>
+where
+    <T as FromStr>::Err: Debug,
+{
+    get_input(filename)
+        .iter()
+        .map(|i| i.parse().unwrap())
+        .collect::<HashSet<T>>()
 }
